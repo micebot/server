4753ae289f7071b305e7e237739fde410ba53d20
Renan Gomes <renan.gomes@live.com>
2020-10-15 11:35:25 -0300

    Removed a inexistent scenario test case.

--- test/unit/routes/test_orders.py
+++ test/unit/routes/test_orders.py
@@ -16,38 +16,6 @@ class TestGetAllOrders(TestRoute):
 
     @patch("server.routes.orders.repo.get_orders_count")
     @patch("server.routes.orders.repo.get_orders")
-    def test_should_return_404_when_there_are_no_orders_registered(
-        self, get_orders, get_orders_count
-    ):
-        get_orders.return_value = None
-
-        response = self.client.get(
-            "/orders",
-            params={
-                "skip": self.skip,
-                "limit": self.limit,
-                "moderator": self.moderator,
-                "owner": self.owner,
-                "desc": self.desc
-            },
-        )
-        self.assertEqual(404, response.status_code)
-        self.assertEqual(
-            {"detail": "No orders registered yet."}, response.json()
-        )
-
-        get_orders_count.assert_not_called()
-        get_orders.assert_called_with(
-            db=self.db,
-            skip=self.skip,
-            limit=self.limit,
-            moderator=self.moderator,
-            owner=self.owner,
-            desc=self.desc
-        )
-
-    @patch("server.routes.orders.repo.get_orders_count")
-    @patch("server.routes.orders.repo.get_orders")
     def test_should_return_200_with_entities(
         self, get_orders, get_orders_count
     ):
